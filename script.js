c23b9404b20cb1da7ffd9cca0e23ce4054c6a988
saintberhe68-byte <saintberhe68@gmail.com>
2026-08-22 01:44:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+
+  const menu = document.getElementById("navLinks");
+
+  menu.classList.toggle("active");
+
+}
+
+
+function closeMenu() {
+
+  const menu = document.getElementById("navLinks");
+
+  menu.classList.remove("active");
+
+}
